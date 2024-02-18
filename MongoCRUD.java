041beabafbb8baf30dbf924b513ee761fce4ba13
jacobomedina <jpm7494@psu.edu>
2024-02-18 18:56:17 -0500

    starting Mongo

---
+++ MongoCRUD.java
@@ -0,0 +1,8 @@
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.FindIterable;
+import org.bson.Document
+public class MongoCRUD {
+}
